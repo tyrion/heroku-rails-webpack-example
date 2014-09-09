a55bc091cdd85b1e74eda0df45c3e603f2f84a7a
Germano Gabbianelli <tyrion.mx@gmail.com>
2014-09-09 17:35:38 +0200

    Added some example require calls to index.js
    I used jquery and react as two example libs. I installed them with:
    
      bower install jquery --save
      npm install react --save
    
    I also added a simple "hello.js" CommonJS module.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+// local 'hello' CommonJS module
+var hello = require('hello');
+
+// jquery installed with bower
+var $ = require('jquery');
+
+// react installed with npm
+var React = require('react');
+
+
+console.log("Hello:", hello);
+console.log("jQuery version:", $.fn.jquery);
+console.log("React version:", React.version);
